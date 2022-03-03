bbc7a5f360af6a3624678219744d88bbe177b52c
BigMike-Champ <54716786+BigMike-Champ@users.noreply.github.com>
2022-03-02 19:00:30 -0500

    Update DHCP_ASSIGN.sh

--- files/DHCP_ASSIGN.sh
+++ files/DHCP_ASSIGN.sh
@@ -2,7 +2,7 @@
 
 #Purpose: To  carve the DHCP address and apply it to the Cockpit.conf file
 # Author: Michael Verdi
-IP_ADD=$(hostname -I | cut -f1 -d' ')
+IP_ADD=$(hostname | cut -f1 -d' ')
 
 sed -i "s/ServerAlias/ServerAlias $IP_ADD/" /etc/apache2/sites-available/cockpit.conf
 
